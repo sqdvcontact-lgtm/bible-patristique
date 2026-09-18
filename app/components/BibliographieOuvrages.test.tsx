1d0cf8f14b2d0da3d00072501320dacdbfbc2b8e
sqdvcontact-lgtm <sqdv.contact@gmail.com>
2026-09-18 10:46:36 +0200

    Update bibliography list author rendering test

--- app/components/BibliographieOuvrages.test.tsx
+++ app/components/BibliographieOuvrages.test.tsx
@@ -124,7 +124,7 @@ describe('la pièce « Du même auteur »', () => {
 })
 
 describe('une autre liste d’ouvrages', () => {
-  it('rend le nom de famille en petites capitales, depuis la donnée', () => {
+  it('rend le nom de famille en romain, depuis la donnée', () => {
     const autre = bibliographieDesBlocs(
       ENTREES_DU_MEME_AUTEUR.map((entree) => ({ ...entree, piece_key: 'bibliographie-generale' })),
       BLOCS_DU_MEME_AUTEUR,
@@ -138,7 +138,7 @@ describe('une autre liste d’ouvrages', () => {
     // ⛔ Les petites capitales sont SÉMANTIQUES : une classe que la feuille
     // compose, jamais une chaîne passée en capitales par le rendu.
     expect(html).toContain(
-      '<span class="cs-apparat-bibliographie__nom-auteur" data-champ="nom_famille">Fillion</span>',
+      '<span class="cs-apparat-bibliographie__auteur" data-champ="nom_famille">Fillion</span>',
     )
     expect(html).not.toContain('FILLION')
   })
